94f7fa7eeadf9cae220edc5b519124159d2f9db2
ltang15 <62497601+ltang15@users.noreply.github.com>
2020-09-04 01:31:37 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+ /*
+ * Project: Iterated list
+ * Purpose: Builing a iterated class using nested class, iterator and node struct
+ * Notes: Inputting command for the navigation test
+ *
+ * /
+ 
+#include <iostream>
+#include "list.h"
+#include "node.h"
+#include "test.h"
+using namespace std;
+
+int main() {
+
+    cout << endl << endl << endl;
+     test();
+//    test_struct();
+
+    cout << "=============END=============" << endl;
+    return 0;
+}
